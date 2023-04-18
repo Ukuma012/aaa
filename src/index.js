e0745e3ef231f3435d26ee40de9fdd933265e3e5
Ukuma012 <Ukuma012@gmail.com>
2023-04-18 12:47:28 +0900

    position

--- src/index.js
+++ src/index.js
@@ -67,10 +67,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
+      poshistory: [
+        {
+          pos: Array(2).fill(0),
+        },
+      ],
       stepNumber: 0,
       xIsNext: true,
-      lastRow: 0,
-      lastCol: 0,
     };
   }
 
@@ -78,22 +81,36 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
+    const poshistory = this.state.poshistory.slice(
+      0,
+      this.state.stepNumber + 1
+    );
+    const poscurrent = poshistory[poshistory.length - 1];
+    const position = poscurrent.pos.slice();
+
     const col = (i % 3) + 1;
     const row = Math.floor(i / 3) + 1;
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    position[0] = row;
+    position[1] = col;
+
     this.setState({
       history: history.concat([
         {
           squares: squares,
         },
       ]),
+      poshistory: poshistory.concat([
+        {
+          pos: position,
+        },
+      ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
-      lastRow: row,
-      lastCol: col,
     });
   }
 
@@ -107,6 +124,8 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
+    const poshistory = this.state.poshistory;
+    const poscurrent = poshistory[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -125,7 +144,8 @@ class Game extends React.Component {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
-    let where = '(' + this.state.lastRow + ' , ' + this.state.lastCol + ')';
+    let position;
+    position = '(' + poscurrent.pos[0] + ' , ' + poscurrent.pos[1] + ')';
 
     return (
       <div className='game'>
@@ -137,7 +157,7 @@ class Game extends React.Component {
         </div>
         <div className='game-info'>
           <div>{status}</div>
-          <div>{where}</div>
+          <div>{position}</div>
           <ol>{moves}</ol>
         </div>
       </div>
